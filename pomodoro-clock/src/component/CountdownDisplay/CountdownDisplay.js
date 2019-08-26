db651b4539169ea3cb8d3b562a7df78d83b6a7dd
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-26 15:31:45 -0400

    update style

--- pomodoro-clock/src/component/CountdownDisplay/CountdownDisplay.js
+++ pomodoro-clock/src/component/CountdownDisplay/CountdownDisplay.js
@@ -8,20 +8,20 @@ import PauseButton from '../../lib/images/pause-bars.svg';
 export default function CountdownDisplay(props) {
 
     //convert total seconds into strings of minutes and seconds
-    let minutes = ((Math.floor(props.seconds / 60)) + '').padStart(2,'0')
-    let seconds = ((props.seconds % 60) + '').padStart(2,'0')
+    let minutes = ((Math.floor(props.seconds / 60)) + '')
+    let seconds = ((props.seconds % 60) + '').padStart(2, '0')
 
-    
+
     return (
         <div className='countdown-display'>
             <h2>{props.activeTimer}</h2>
-            <p className='time-display'>{`${minutes}:${seconds}`}</p>
-            <Button 
+            <p className='time-display'><span className='time-number'>{minutes}</span>:<span className='time-number'>{seconds}</span></p>
+            <Button
                 callFunction={console.log}
                 button={PauseButton}
                 altText={'two square horizontal bars'}
             />
-            <Button 
+            <Button
                 callFunction={console.log}
                 button={PlayButton}
                 altText={'triangle rotated so that top is pointing left'}
