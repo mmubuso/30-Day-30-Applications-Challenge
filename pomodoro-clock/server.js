9768f7490b83a7bea778bbf29b6f18640d3b5b3b
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-26 19:08:45 -0400

    add server file

---
+++ pomodoro-clock/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 9000 
+
+app.listen(PORT);
