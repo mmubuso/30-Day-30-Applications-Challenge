1839339ac7aa4a08c54b0a5c0bfe9801a115f722
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-09-09 22:42:41 -0400

    adjust style for navbar

--- weathery/App.js
+++ weathery/App.js
@@ -6,14 +6,15 @@
  * @flow
  */
 
-import Search from './containers/Search/Search'
+import Search from './containers/Search/Search';
+import City from './containers/City/Cityy';
 import React, { useState } from "react";
 import { NativeRouter, Route, Link, Switch } from "react-router-native";
 import {
   StyleSheet,
-  ScrollView,
+  Image,
   View,
-  Text,
+  Text
 } from 'react-native';
 
 
@@ -21,15 +22,26 @@ const App = () => {
 
   //Array that containes the list of cities by their Id
   const [cities, updateCities] = useState([])
+  const [showCityName, ToggleShowCityName] = useState(false)
+  const [activeCity, updateActiveCity] = useState('Auburn')
+
+
+  // Pass props to components
 
   return (
     <NativeRouter >
       <View style={Styles.app} >
+
         <Switch>
           <Route exact path="/" component={Search} />
           {/* <Route path="/cityList" component={cityList} /> */}
-          {/* <Route path="/weather/:cityId" component={City} /> */}
+          <Route path="/weather/:cityId/:cityName" component={City} />
         </Switch>
+      </View>
+      <View style={Styles.navbar}>
+        <Text style={Styles.navText}>Cities</Text>
+        <Link to='/' ><Text style={Styles.navText}>+</Text></Link>
+
       </View>
     </NativeRouter>
   );
@@ -37,12 +49,22 @@ const App = () => {
 
 const Styles = StyleSheet.create({
   app: {
-    paddingTop: 50,
-    paddingLeft: 10,
-    paddingRight: 10,
     display: 'flex',
     flex: 1,
-    height:'100%' 
+    height: '100%'
+  },
+  navbar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#FE5F55',
+    height: 55,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+  navText: {
+    fontSize: 30,
+    color: 'whitesmoke'
   }
 });
 
