d49ad943b142a5d448519a9255edc1ddbaecfdf3
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-09-20 19:10:00 -0400

    add files

---
+++ book-shelf/src/components/MultiBooks/MultiBooks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MultiBooks() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ book-shelf/src/containers/Library/Library.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Library extends Component {
+    render() {
+        return (
+            <div>
+                f
+            </div>
+        )
+    }
+}
