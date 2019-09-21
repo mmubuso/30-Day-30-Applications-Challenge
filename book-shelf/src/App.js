4b62dcab693ede0f623887a07acb0e1606a57fb4
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-09-20 20:56:35 -0400

    add book list

--- book-shelf/src/App.js
+++ book-shelf/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Library from './containers/Library/Library';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+
+    <Router className="App">
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+      </nav>
+      <Switch>
+        <Route exact path='/' component={Library} />
+      </Switch>
+    </Router>
+
   );
 }
 
--- book-shelf/src/containers/Library/Library.js
+++ book-shelf/src/containers/Library/Library.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import MultiBooks from '../../components/MultiBooks/MultiBooks';
 
 export default class Library extends Component {
+    state = {
+        libraries: []
+    }
+
+    componentDidMount() {
+        this.getBooksFromServer()
+    }
+
+    //Get books and store it in state data from server
+    getBooksFromServer = () => {
+        axios.get('http://localhost:8000/api/v1/books')
+            .then(results => {
+                this.setState({ libraries: results.data })
+                console.log(results)
+            })
+            .catch(err => console.log(err))
+    }
+    
+
     render() {
+        let books = this.state.libraries.map(book => {
+            return (
+                <MultiBooks 
+                    key={book.ID}
+                    id={book.ID}
+                    title={book.Title}
+                    author={`${book.Author.FirstName} ${book.Author.LastName}`}
+                />
+            )
+        })
         return (
-            <div>
-                f
+            <div className="container">
+                <div className="jumbotron">
+                    <h1>Musiteli Mubuso</h1>
+                    {books}
+                </div>
             </div>
         )
     }
 }
+
